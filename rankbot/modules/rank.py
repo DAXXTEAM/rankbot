61502ddf07ec8b22b6ee535e0e5262fdf57bc403
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-30 11:58:08 +0530

    Update and rename rank to rank.py

--- rankbot/modules/rank.py
+++ rankbot/modules/rank.py
@@ -1,14 +1,15 @@
 from pyrogram import Client, filters
 from pymongo import MongoClient
-from pyrogram.types import Message
-import os
-from rankbot import rankbot as app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
+from rankbot import rankbot as app
 
 # --------------------------------------------------------------------------
 mongo_uri = "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority"
 database_name = "MONGODB"
 # ------------------------------------------------------------------------------
+
+
 # ------------------------------------------------------------------------------
 
 mongo_client = MongoClient(mongo_uri)
@@ -17,34 +18,29 @@ top_members_collection = db["rank_db"]
 
 user_data = {}
 
-# --------------------------------------------------------------------------------------------
-# -------------------------------------------------------------------------------------------
+# ----------------
 
 
 
-# -------------
-@app.on_message(filters.command("ranking"))
+@app.on_message(filters.command("top10"))
 def top_members(_, message):
     top_members = top_members_collection.find().sort("total_messages", -1).limit(10)
     
     response = "📈 𝖫𝖤𝖠𝖣𝖤𝖱𝖡𝖮𝖠𝖱𝖣\n"
     for idx, member in enumerate(top_members, start=1):
         user_id = member["_id"]
+        
+        try:
+            user = app.get_users(user_id)
+            first_name = user.first_name if user.first_name else "Unknown"
+        except PeerIdInvalid:
+            first_name = "Unknown"
+        
         total_messages = member["total_messages"]
-        
-        # Retrieve user information
-        user = app.get_users(user_id)
-        if user:
-            user_info = f"{idx}. {user.mention}, •{total_messages}\n"
-        else:
-            # If user info is not available, use the user ID as the first name
-            user_info = f"{idx}. Unknown User ({user_id}), •{total_messages}\n"
-        
+        user_info = f"{idx}. 👤{first_name}, •{total_messages}\n"
         response += user_info
 
     message.reply_text(response)
-
-
 
 
 @app.on_message()
@@ -55,7 +51,3 @@ def handle_messages(_, message):
     user_data[user_id]["total_messages"] += 1
     
     top_members_collection.update_one({"_id": user_id}, {"$inc": {"total_messages": 1}}, upsert=True)
-
-
-# GROUPRANKINGROBOT---------------------------------------------------
-# GROUPRANKINGROBOT---------------------------------------------------
